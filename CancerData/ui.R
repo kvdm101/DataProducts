2896f3a4d149b33949d5909645c7396dc60f22ce
Karin <kvdm101@gmail.com>
2015-10-24 16:30:53 +0200

    Signed-off-by: Karin <kvdm101@gmail.com>

--- CancerData/ui.R
+++ CancerData/ui.R
@@ -1,30 +1,29 @@
 library(shiny)
 library(ggplot2)
 library(MASS)
-
+require(markdown)
 dataset <- esoph
 
 #shinyUI(pageWithSidebar(
 shinyUI(fluidPage(
-    headerPanel("Smoking, Alcohol and (O)esophageal Cancer"),
-    
-    sidebarPanel(
+    titlePanel(""),
+        headerPanel("Smoking, Alcohol and (O)esophageal Cancer"),
         
-        selectInput('x', 'X Axis', names(dataset[,1:3])),
-        selectInput('y', 'Y Axis', names(dataset[,4:5]), names(dataset[,4:5])),
-        selectInput('color', 'Color', c('None', names(dataset))),
-        selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
-        selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
-    ),
-    
-    helpText("Note: The variables used in this application is based on the Esoph data package and a full decription can be found in the R Datasets Package.
-             The application is only to be used to explore the data."),
-    
-    mainPanel(
-        plotOutput('plot')
+        sidebarPanel(
+            
+            selectInput('x', 'X Axis', names(dataset[,1:3])),
+            selectInput('y', 'Y Axis', names(dataset[,4:5]), names(dataset[,4:5])),
+            selectInput('color', 'Color', c('None', names(dataset))),
+            selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
+            selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
+        ),
         
-        
-    )
+        mainPanel(
+            tabsetPanel(type = "tabs", 
+                  tabPanel("Plot", plotOutput("plot")), 
+                  tabPanel("Documentation",includeMarkdown("DataProductsSlide.md")
+        )
+        ))
 ))
 
 
